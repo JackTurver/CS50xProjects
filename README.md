6dbf0da8a0d68f2aa1a02143d4ec1604cece77e3
JackTurver <jackturver1@gmail.com>
2023-01-16 15:51:54 +0000

    Projects from week 1 upload

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # CS50xProjects
 
 //This repository is used to show my CS50x Harvard course projects
-//The first commited file was a rock paper scissors python file in order to set the repository up and is not included in the course projects
 //I am hoping once I have completed this course and have learned the full syntax of a programming language to start a role as a software engineer
 //The first languages I will attempt to master will molst likely be Python, JavaScript and C
+
 //The LOTR game.sb3 file is a basic scratch game. This is the project submission for the first week of the CS50x course.
+
+//The .c files are projects from the second week of the CS50x course.
---
+++ mario.c
@@ -0,0 +1,50 @@
+// Both needed to make sure we have all the libraries we need
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+   // this integer is to get the height of the pyramids
+   // integer needs to be between 1 and 8 so need a do while loop
+   // the while loop needs an or conitional ||
+   int h;
+   do
+   {
+      h = get_int("Height: ");
+   } while ((h < 1) || (h > 8));
+
+   // initializing n to be h so that we can hold the value of h
+   // n will be reduced by one each time a new row is created, this is when i increases by 1
+   int n = h;
+
+   // for loop going up to the h value chosen
+   for (int i = 0; i < h; i++)
+   {
+      // initializing w so that we can set it to 0 in the inner loop once it has been used to output the right number of spaces before the first #
+      // setting w = 0 means we can exit this part of the loop until we move onto the new row where we can reset the value of w
+      // basically an exit value or stop this function
+      int w = (n - 1);
+      // second for loop to draw the correct number of # per row
+      for (int j = 0; j < (i + 1); j++)
+      {
+         // inner loop to draw correct number of spaces before the first #
+         for (int k = w; k > 0; k--)
+         {
+            printf(" ");
+         }
+         w = 0;
+         printf("#");
+      }
+      // print the 2 spaces in the middle of the blocks
+      printf("  ");
+      // last loop copy so that it will print the correct number of # after the 2 spaces
+      for (int j = 0; j < (i + 1); j++)
+      {
+         printf("#");
+      }
+      // create new line after we have printed the correct number of # and spaces
+      printf("\n");
+      // reduce n by one within the loop so that we can correctly reset w
+      n--;
+   }
+}
